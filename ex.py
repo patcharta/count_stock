a4e6934ac229426d3a45420e563f561eacbc64ad
patcharta <patcharamon.t@kgcorp.com>
2024-05-28 12:52:25 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -30,7 +30,6 @@ def download_data(df, username, department_name, login_time, product_quantity):
     
     login_info = pd.DataFrame({
         'Username': [username],
-       
         'Login Time': [login_time]
     })
     login_info.to_excel(writer, index=False, sheet_name='LoginInfo')
@@ -69,6 +68,7 @@ def save_to_database(product_data_list):
         st.success("Data saved successfully!")
     except pyodbc.Error as e:
         st.error(f"Error inserting data: {e}")
+        st.error(f"Original error message: {str(e.args)}")
 
 def app():
     if 'logged_in' not in st.session_state:
@@ -88,110 +88,117 @@ def app():
 
         if st.session_state.selected_whcid is None:
             st.subheader("เลือก WHCID")
-            with pyodbc.connect(conn_str) as conn:
-                whcid_query = '''
-                SELECT y.WHCID, y.NAME_TH 
-                FROM ERP_WAREHOUSES_CODE y
-                '''
-                whcid_df = pd.read_sql(whcid_query, conn)
-                selected_whcid = st.selectbox("เลือก WHCID:", options=whcid_df['WHCID'] + ' - ' + whcid_df['NAME_TH'])
-                
-                if st.button("👉 Enter WHCID"):
-                    st.session_state.selected_whcid = selected_whcid
+            try:
+                with pyodbc.connect(conn_str) as conn:
+                    whcid_query = '''
+                    SELECT y.WHCID, y.NAME_TH 
+                    FROM ERP_WAREHOUSES_CODE y
+                    '''
+                    whcid_df = pd.read_sql(whcid_query, conn)
+                    selected_whcid = st.selectbox("เลือก WHCID:", options=whcid_df['WHCID'] + ' - ' + whcid_df['NAME_TH'])
+                    
+                    if st.button("👉 Enter WHCID"):
+                        st.session_state.selected_whcid = selected_whcid
+            except pyodbc.Error as e:
+                st.error(f"Error fetching WHCID data: {e}")
+                st.error(f"Original error message: {str(e.args)}")
         else:
             st.write(f"คุณเลือก WHCID: {st.session_state.selected_whcid}")
 
             st.subheader("ค้นหาสินค้า")
             
-            with pyodbc.connect(conn_str) as conn:
-                product_query = '''
-                SELECT x.ITMID, x.NAME_TH, x.MODEL, x.EDITDATE
-                FROM ERP_ITEM_MASTER_DATA x
-                WHERE x.EDITDATE IS NULL AND x.GRPID IN ('11', '71', '77', '73', '76', '75')
-                '''
-                items_df = pd.read_sql(product_query, conn)
-            
-            selected_product_name = st.selectbox("เลือกสินค้า:", options=items_df['ITMID']  + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'], key='selected_product')
-            st.session_state.selected_product_name = selected_product_name
-            
-            if selected_product_name:
-                st.write(f"คุณเลือกสินค้า: {selected_product_name}")
-                
-                with pyodbc.connect(conn_str) as conn_detail:
-                    query_detail = '''
-                    SELECT 
-                        a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL, a.PHOTONAME, 
-                        b.BRAND_NAME, c.CAB_NAME, d.SHE_NAME, e.BLK_NAME, 
-                        p.WHCID, w.NAME_TH AS WAREHOUSE_NAME, p.BATCH_NO, SUM(p.BALANCE) AS TOTAL_BALANCE
-                    FROM 
-                        ERP_ITEM_MASTER_DATA a 
-                        LEFT JOIN ERP_BRAND b ON a.BRAID = b.BRAID
-                        LEFT JOIN ERP_CABINET c ON a.CABID = c.CABID
-                        LEFT JOIN ERP_SHELF d ON a.SHEID = d.SHEID
-                        LEFT JOIN ERP_BLOCK e ON a.BLKID = e.BLKID
-                        LEFT JOIN ERP_GOODS_RECEIPT_PO_BATCH p ON a.ITMID = p.ITMID
-                        LEFT JOIN ERP_WAREHOUSES_CODE w ON p.WHCID = w.WHCID
-                    WHERE 
-                        a.EDITDATE IS NULL AND
-                        a.GRPID IN ('11', '71', '77', '73', '76', '75') 
-                        AND a.ITMID + ' - ' + a.NAME_TH + ' - ' + a.MODEL = ?
-                        AND p.WHCID = ?
-                    GROUP BY 
-                        a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL, a.PHOTONAME, 
-                        b.BRAND_NAME, c.CAB_NAME, d.SHE_NAME, e.BLK_NAME, 
-                        p.WHCID, w.NAME_TH, p.BATCH_NO
+            try:
+                with pyodbc.connect(conn_str) as conn:
+                    product_query = '''
+                    SELECT x.ITMID, x.NAME_TH, x.MODEL, x.EDITDATE
+                    FROM ERP_ITEM_MASTER_DATA x
+                    WHERE x.EDITDATE IS NULL AND x.GRPID IN ('11', '71', '77', '73', '76', '75')
                     '''
-                    filtered_items_df = pd.read_sql(query_detail, conn_detail, params=(selected_product_name, st.session_state.selected_whcid.split(' - ')[0]))
-            
-                if not filtered_items_df.empty:
-                    st.write("### รายละเอียดสินค้า:")
-                    # Select the required columns to display
-                    display_columns = [
-                        'BRAND_NAME', 'CAB_NAME', 'SHE_NAME', 'BLK_NAME', 
-                        'BATCH_NO', 'TOTAL_BALANCE'
-                    ]
-                    st.dataframe(filtered_items_df[display_columns])
+                    items_df = pd.read_sql(product_query, conn)
+                
+                selected_product_name = st.selectbox("เลือกสินค้า:", options=items_df['ITMID']  + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'], key='selected_product')
+                st.session_state.selected_product_name = selected_product_name
+                
+                if selected_product_name:
+                    st.write(f"คุณเลือกสินค้า: {selected_product_name}")
+                    
+                    with pyodbc.connect(conn_str) as conn_detail:
+                        query_detail = '''
+                        SELECT 
+                            a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL, a.PHOTONAME, 
+                            b.BRAND_NAME, c.CAB_NAME, d.SHE_NAME, e.BLK_NAME, 
+                            p.WHCID, w.NAME_TH AS WAREHOUSE_NAME, p.BATCH_NO, SUM(p.BALANCE) AS TOTAL_BALANCE
+                        FROM 
+                            ERP_ITEM_MASTER_DATA a 
+                            LEFT JOIN ERP_BRAND b ON a.BRAID = b.BRAID
+                            LEFT JOIN ERP_CABINET c ON a.CABID = c.CABID
+                            LEFT JOIN ERP_SHELF d ON a.SHEID = d.SHEID
+                            LEFT JOIN ERP_BLOCK e ON a.BLKID = e.BLKID
+                            LEFT JOIN ERP_GOODS_RECEIPT_PO_BATCH p ON a.ITMID = p.ITMID
+                            LEFT JOIN ERP_WAREHOUSES_CODE w ON p.WHCID = w.WHCID
+                        WHERE 
+                            a.EDITDATE IS NULL AND
+                            a.GRPID IN ('11', '71', '77', '73', '76', '75') 
+                            AND a.ITMID + ' - ' + a.NAME_TH + ' - ' + a.MODEL = ?
+                            AND p.WHCID = ?
+                        GROUP BY 
+                            a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL, a.PHOTONAME, 
+                            b.BRAND_NAME, c.CAB_NAME, d.SHE_NAME, e.BLK_NAME, 
+                            p.WHCID, w.NAME_TH, p.BATCH_NO
+                        '''
+                        filtered_items_df = pd.read_sql(query_detail, conn_detail, params=(selected_product_name, st.session_state.selected_whcid.split(' - ')[0]))
+                
+                    if not filtered_items_df.empty:
+                        st.write("### รายละเอียดสินค้า:")
+                        display_columns = [
+                            'BRAND_NAME', 'CAB_NAME', 'SHE_NAME', 'BLK_NAME', 
+                            'BATCH_NO', 'TOTAL_BALANCE'
+                        ]
+                        st.dataframe(filtered_items_df[display_columns])
 
-                    total_balance = filtered_items_df['TOTAL_BALANCE'].sum()
-                    if total_balance == 0:
-                        st.write("ไม่มีสินค้า")
-                    else:
-                        st.write(f"รวมยอดสินค้าในคลัง: {total_balance}")
-                        
-                        product_quantity = st.session_state.product_quantity
-                        product_quantity = st.number_input(label='จำนวนสินค้า 🛒', min_value=0, value=product_quantity)
-                        remark = st.text_area('Remark')
-
-                        if st.button('👉 Enter') and product_quantity > 0:
-                            product_data = {
-                                'Login_Time': st.session_state.login_time,
-                                'Enter_By': st.session_state.username,
-                                'Product_ID': str(filtered_items_df['ITMID'].iloc[0]),
-                                'Product_Name': str(filtered_items_df['NAME_TH'].iloc[0]),
-                                'Model': str(filtered_items_df['MODEL'].iloc[0]),
-                                'Brand_Name': str(filtered_items_df['BRAND_NAME'].iloc[0]),
-                                'Cabinet': str(filtered_items_df['CAB_NAME'].iloc[0]),
-                                'Shelf': str(filtered_items_df['SHE_NAME'].iloc[0]),
-                                'Block': str(filtered_items_df['BLK_NAME'].iloc[0]),
-                                'Warehouse_ID': str(filtered_items_df['WHCID'].iloc[0]),
-                                'Warehouse_Name': str(filtered_items_df['WAREHOUSE_NAME'].iloc[0]),
-                                'Batch_No': str(filtered_items_df['BATCH_NO'].iloc[0]),
-                                'Purchasing_UOM': str(filtered_items_df['PURCHASING_UOM'].iloc[0]),
-                                'Total_Balance': int(total_balance),
-                                'Quantity': int(product_quantity),
-                                'Remark': remark
-                            }
+                        total_balance = filtered_items_df['TOTAL_BALANCE'].sum()
+                        if total_balance == 0:
+                            st.write("ไม่มีสินค้า")
+                        else:
+                            st.write(f"รวมยอดสินค้าในคลัง: {total_balance}")
                             
-                            st.session_state.product_data.append(product_data)
-                            save_to_database(st.session_state.product_data)
-                            st.write(f"บันทึกข้อมูลสินค้า: {product_quantity} จำนวนสินค้า {st.session_state.selected_product_name}")
+                            product_quantity = st.session_state.product_quantity
+                            product_quantity = st.number_input(label='จำนวนสินค้า 🛒', min_value=0, value=product_quantity)
+                            remark = st.text_area('Remark')
 
-                            if st.session_state.product_data:
-                                product_df = pd.DataFrame(st.session_state.product_data)
-                                download_data(product_df, st.session_state.username, st.session_state.department_name, st.session_state.login_time, product_quantity)
+                            if st.button('👉 Enter') and product_quantity > 0:
+                                product_data = {
+                                    'Login_Time': st.session_state.login_time,
+                                    'Enter_By': st.session_state.username,
+                                    'Product_ID': str(filtered_items_df['ITMID'].iloc[0]),
+                                    'Product_Name': str(filtered_items_df['NAME_TH'].iloc[0]),
+                                    'Model': str(filtered_items_df['MODEL'].iloc[0]),
+                                    'Brand_Name': str(filtered_items_df['BRAND_NAME'].iloc[0]),
+                                    'Cabinet': str(filtered_items_df['CAB_NAME'].iloc[0]),
+                                    'Shelf': str(filtered_items_df['SHE_NAME'].iloc[0]),
+                                    'Block': str(filtered_items_df['BLK_NAME'].iloc[0]),
+                                    'Warehouse_ID': str(filtered_items_df['WHCID'].iloc[0]),
+                                    'Warehouse_Name': str(filtered_items_df['WAREHOUSE_NAME'].iloc[0]),
+                                    'Batch_No': str(filtered_items_df['BATCH_NO'].iloc[0]),
+                                    'Purchasing_UOM': str(filtered_items_df['PURCHASING_UOM'].iloc[0]),
+                                    'Total_Balance': int(total_balance),
+                                    'Quantity': int(product_quantity),
+                                    'Remark': remark
+                                }
+                                
+                                st.session_state.product_data.append(product_data)
+                                save_to_database(st.session_state.product_data)
+                                st.write(f"บันทึกข้อมูลสินค้า: {product_quantity} จำนวนสินค้า {st.session_state.selected_product_name}")
 
-                else:
-                    st.warning("ไม่พบข้อมูลสินค้าที่เลือก")   
+                                if st.session_state.product_data:
+                                    product_df = pd.DataFrame(st.session_state.product_data)
+                                    download_data(product_df, st.session_state.username, st.session_state.department_name, st.session_state.login_time, product_quantity)
+
+                    else:
+                        st.warning("ไม่พบข้อมูลสินค้าที่เลือก")
+            except pyodbc.Error as e:
+                st.error(f"Error fetching product data: {e}")
+                st.error(f"Original error message: {str(e.args)}")
             
             if st.button('📤 Logout'):
                 st.session_state.logged_in = False
@@ -209,11 +216,9 @@ def app():
         password = st.text_input("Password", type="password")
         
         if st.button("Login"):
-            user_info = check_credentials(username, password)
-            if user_info is not None:
+            if check_credentials(username, password):
                 st.session_state.logged_in = True
                 st.session_state.username = username
-                #st.session_state.department_name = user_info['DEPARTMENT_NAME']
                 st.session_state.login_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 st.success(f"🎉🎉 Welcome {username}")
                 time.sleep(1)
@@ -223,3 +228,4 @@ def app():
 
 if __name__ == "__main__":
     app()
+
