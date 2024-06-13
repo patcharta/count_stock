9715663a3accd91ac12ff52440a667eb8169accb
patcharta <patcharamon.t@kgcorp.com>
2024-06-13 16:22:40 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -173,7 +173,7 @@ def select_product(company, conn_str):
                     st.write(f"Matching Product: {selected_product_name}")
                     return selected_product_name, matching_products.iloc[0]
     else:
-            st.write("No QR code detected.")
+        st.write("No QR code detected.")
 
     except cv2.error as e:
         st.error(f"OpenCV Error: {e}")
