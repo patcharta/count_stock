c0a02d364759acb4ece1f2131a6898fc49abd85c
patcharta <patcharamon.t@kgcorp.com>
2024-06-13 16:15:12 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -187,6 +187,22 @@ def select_product(company, conn_str):
 
     return None, None
 
+def get_image_url(product_name):
+    try:
+        query = "+".join(product_name.split())
+        url = f"https://www.google.com/search?tbm=isch&q={query}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        image_element = soup.find("img", {"src": re.compile("https://.*")})
+        image_url = image_element["src"] if image_element else None
+        return image_url
+    except Exception as e:
+        st.error(f"Error fetching image: {e}")
+        return None
+
 def count_product(selected_product_name, selected_item, conn_str):
     filtered_items_df = load_data(selected_product_name, st.session_state.selected_whcid, conn_str)
     total_balance = 0
