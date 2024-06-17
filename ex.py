4746cae01b6700089a5a26d793b9c7b411033a49
patcharta <patcharamon.t@kgcorp.com>
2024-06-17 10:02:30 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -128,32 +128,32 @@ def fetch_products(company):
 
 col1, col2 = st.columns(2)
 with col1:
-def select_product(company):
-    st.write("ค้นหาสินค้า 🔎")
-    items_df = fetch_products(company)
-    items_options = list(items_df['ITMID'] + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'] + ' - ' + items_df['BRAND_NAME'])
+    def select_product(company):
+        st.write("ค้นหาสินค้า 🔎")
+        items_df = fetch_products(company)
+        items_options = list(items_df['ITMID'] + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'] + ' - ' + items_df['BRAND_NAME'])
 
-    # Adding CSS for word wrap
-    st.markdown("""
-        <style>
-        .wrap-text .css-1wa3eu0 {
-            white-space: normal !important;
-            overflow-wrap: anywhere;
-        }
-        </style>
-        """, unsafe_allow_html=True)
+        # Adding CSS for word wrap
+        st.markdown("""
+            <style>
+            .wrap-text .css-1wa3eu0 {
+                white-space: normal !important;
+                overflow-wrap: anywhere;
+            }
+            </style>
+            """, unsafe_allow_html=True)
 
-    selected_product_name = st.selectbox("เลือกสินค้า", options=items_options, index=None, key='selected_product')
+        selected_product_name = st.selectbox("เลือกสินค้า", options=items_options, index=None, key='selected_product')
 
-    if selected_product_name:
-        selected_item = items_df[items_df['ITMID'] + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'] + ' - ' + items_df['BRAND_NAME'] == selected_product_name]
-        st.write(f"คุณเลือกสินค้า: {selected_product_name}")
-        st.markdown("---")
-        return selected_product_name, selected_item
-    else:
-        return None, None  # Return None, None if no product is selected
+        if selected_product_name:
+            selected_item = items_df[items_df['ITMID'] + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'] + ' - ' + items_df['BRAND_NAME'] == selected_product_name]
+            st.write(f"คุณเลือกสินค้า: {selected_product_name}")
+            st.markdown("---")
+            return selected_product_name, selected_item
+        else:
+            return None, None  # Return None, None if no product is selected
 
-    with col2:
+with col2:
     # QR code scanner
     qr_code = qrcode_scanner(key="qr_code_scanner")
     if qr_code:
