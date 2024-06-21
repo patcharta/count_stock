b847753322fa6f232cee0caa4ac68b2ded690d5e
patcharta <patcharamon.t@kgcorp.com>
2024-06-21 08:42:17 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from streamlit_qrcode_scanner import qrcode_scanner
 
-qr_code = qrcode_scanner(key='qrcode_scanner')
+# QR code scanner with 1:1 aspect ratio for square shape
+qr_code = qrcode_scanner(key='qrcode_scanner', aspect_ratio='1:1')
 
 if qr_code:
     st.write(qr_code)
