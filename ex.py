53e50ecf015cdd7eed5c39d2386cf26e3f91aef1
patcharta <patcharamon.t@kgcorp.com>
2024-06-21 11:25:07 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -268,21 +268,17 @@ def select_product_by_qr(company):
     st.write("ค้นหาสินค้า 🔍")
     items_df = fetch_products(company)
     
-    # Check if qr_code_scanner is in session_state and not None
-    if 'qr_code_scanner' in st.session_state and st.session_state.qr_code_scanner:
-        qr_code = st.session_state.qr_code_scanner
+    qr_code = qrcode_scanner(key="qr_code_scanner")
+    if qr_code:
         st.write(f"QR Code detected: {qr_code}")
         selected_product = items_df[items_df['ITMID'] == qr_code]
         if not selected_product.empty:
             selected_product_name = selected_product.iloc[0]['ITMID'] + ' - ' + selected_product.iloc[0]['NAME_TH'] + ' - ' + selected_product.iloc[0]['MODEL'] + ' - ' + selected_product.iloc[0]['BRAND_NAME']
             st.markdown(f'คุณเลือกสินค้า: <strong style="background-color: #ffa726; padding: 2px 5px; border-radius: 5px; color: black;">{selected_product_name}</strong>', unsafe_allow_html=True)
             st.markdown("---")
-            # Clear the qr_code_scanner from session_state after use
-            del st.session_state['qr_code_scanner']
             return selected_product_name, selected_product
 
     return None, None
-
                 
 def login_section():
     st.write("## Login 🚚")
