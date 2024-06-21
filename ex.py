ecc9bb7f05c99281a601a26642c51e92d91127b4
patcharta <patcharamon.t@kgcorp.com>
2024-06-21 17:06:50 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -280,13 +280,9 @@ def select_product_by_qr(company):
                 selected_product_name = selected_product.iloc[0]['ITMID'] + ' - ' + selected_product.iloc[0]['NAME_TH'] + ' - ' + selected_product.iloc[0]['MODEL'] + ' - ' + selected_product.iloc[0]['BRAND_NAME']
                 st.markdown(f'คุณเลือกสินค้า: <strong style="background-color: #ffa726; padding: 2px 5px; border-radius: 5px; color: black;">{selected_product_name}</strong>', unsafe_allow_html=True)
                 st.markdown("---")
-                if 'qr_code_scanner' in st.session_state:
-                    del st.session_state['qr_code_scanner']  # Clear the QR code scanner state
-                st.experimental_rerun()  # Rerun the app to reflect the selected product
                 return selected_product_name, selected_product
 
     return None, None
-
                 
 def login_section():
     st.write("## Login 🚚")
